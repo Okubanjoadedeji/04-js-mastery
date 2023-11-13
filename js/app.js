0e0a6ba48c282e418bd612eb60cbff62ee9a9aa6
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-13 16:18:18 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Booleans
+const ifCool = true;
+
+console.log(ifCool);
+
+if (ifCool) {
+  console.log("Hi man, you're cool");
+} else {
+  console.log('Oh, hi .....');
+}
+
+
+// Null 
+let age = null;
+
+console.log(age);
+
+undefined 
+let y;
+
+console.log(y);
